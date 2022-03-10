a1e7dc26b9f9392bc2637f979f7d291fa1fd3c96
Dialtor <diepresiga@gmail.com>
2022-03-10 12:18:10 -0500

    Fixing Responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,15 +52,4 @@ filterContainer.addEventListener("click", (event) =>{
   }
 });
 
-var cntWd, sldWd, tb;
-$(function () {
-  cntWd = $("#container").innerWidth();
-  tb = $("#thumbs");
-  sldWd = tb.outerWidth();
 
-  $("#container").mousemove(function (e) {
-    tb.css({
-      left: ((cntWd - sldWd) * (e.pageX / cntWd).toFixed(3)).toFixed(1) + "px"
-    });
-  });
-});
